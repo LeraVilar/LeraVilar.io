0a2c6f3be9eb33383f164beda872888201edf144
Дмитрий Красиков <noviynomer@MacBook-Air-Dmitrij.local>
2020-10-01 15:05:39 +0400

    i3

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.coments__slider').slick({
+        dots:false,
+        slidesToShow: 2,
+        autoplay:true,
+        speed: 1000,
+        autoplaySpeed: 1000,
+    });
+
+});
+
+
